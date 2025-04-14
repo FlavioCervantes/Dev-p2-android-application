8504e861a48450b3d4a2ae69fa357d2b95fd1544
ummonica <monicaum2@yahoo.com>
2025-04-13 19:42:28 -0700

    Finished active directory database for now. Check for anything missing and any issues implementing with other databases.

--- app/src/main/java/com/example/dev_p2_android_application/database/TriviaGameDatabase.java
+++ app/src/main/java/com/example/dev_p2_android_application/database/TriviaGameDatabase.java
@@ -1,8 +1,15 @@
 package com.example.dev_p2_android_application.database;
 
-import androidx.room.Database;
+import android.content.Context;
 
-@Database(entities = {ActiveDirectory.class}, version = 1)
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Database(entities = {ActiveDirectory.class}, version = 1, exportSchema = false)
 public abstract class TriviaGameDatabase extends RoomDatabase {
     public abstract ActiveDirectoryDAO activeDirectoryDAO();
 }
